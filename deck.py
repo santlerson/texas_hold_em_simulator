9604d6bf931ae913ae79024f1d3749f510322db5
Shmoosey Antlerson <shmooseyantlerson@gmail.com>
2023-10-26 15:56:32 +0300

    Changed source of randomness for deck shuffling to be more secure

--- deck.py
+++ deck.py
@@ -5,7 +5,8 @@ import random
 class Deck:
     def __init__(self):
         self.cards = [Card(rank, suit) for rank in range(2, 15) for suit in range(4)]
-        random.shuffle(self.cards)
+        gen = random.SystemRandom()  # Secure random generator
+        gen.shuffle(self.cards)
 
     def draw(self, n=1):
         return [self.cards.pop() for _ in range(n)]
